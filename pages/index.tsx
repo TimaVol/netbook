21bf06e3f0d17d113d32f636366c1dfa83e28ef2
TimaVol <tima.voloshuk@gmail.com>
2023-03-08 14:52:21 +0200

    done with WeAreConnecting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import AdMobApps from "@/components/AdMobApps";
 import Community from "@/components/Community";
 import Members from "@/components/Members";
 import News from "@/components/News";
+import WeAreConnecting from "@/components/WeAreConnecting";
 
 export default function Home() {
   return (
     <>
+      <WeAreConnecting />
       <Community />
       <Members />
       <AdMobApps />
